2fd0b299f829926f33575a15fdec05a390cdd417
harumidiv <42649032+harumidiv@users.noreply.github.com>
2024-04-25 11:53:03 +0900

    SMPのプロジェクト構成にしました

---
+++ SpeculativeStockExcavation/SpeculativeStockExcavation/SpeculativeStockExcavationApp.swift
@@ -0,0 +1,18 @@
+//
+//  SpeculativeStockExcavationApp.swift
+//  SpeculativeStockExcavation
+//
+//  Created by 佐川 晴海 on 2024/04/25.
+//
+
+import SwiftUI
+import SpeculativeStockExcavationPackage
+
+@main
+struct SpeculativeStockExcavationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeculativeStockExcavationPackage/Sources/SpeculativeStockExcavationPackage/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpeculativeStockExcavation
+//
+//  Created by 佐川 晴海 on 2024/04/25.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+
+    public init() {}
+}
+
+#Preview {
+    ContentView()
+}
